ce695ac9a2be1f9dd5f588da361509d0528323c2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-13 21:34:10 +0900

    explain

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 // Toán tử ++ -- 
 // Tiền tố và hậu tố - Prefix & Postfix 
+
+var a = 6
+
+var output = a++
+console.log(output)
+
+// ++a:
+// Việc 1:  +1 cho a, a = a +1 => a = 7
+// Việc 2: Trả về a sau khi được + 1 
+
+// a--:
+// Việc 1: copy biến a ra biến 'a copy', 'a copy' = 6
+// Việc 2: trừ 1 của a, a = a - 1 => a = 5
+// Việc 3: trả về 'a copy'
