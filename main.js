a00addcce7917400a84b9c76c56cac2f675bd586
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-13 21:25:07 +0900

    Lesson 4

--- main.js
+++ main.js
@@ -1,4 +1,2 @@
-setInterval(function() {
-    console.log('Day la log' + Math.random())
-},1000);
-// thuc thi lien tuc sau 1 khoang thoi gian nhat dinh 
+// Toán tử ++ -- 
+// Tiền tố và hậu tố - Prefix & Postfix 
