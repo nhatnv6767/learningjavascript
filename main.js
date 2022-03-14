04b8656d80234bcab5cf292eae87a11df9b538d6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-14 23:10:45 +0900

    lastIndexOf

--- main.js
+++ main.js
@@ -3,5 +3,5 @@
 
 var myString = "Hoc JS tai JS JS FF";
 
-// Tim index cua JS bat dau tu index thu 6
-console.log(myString.indexOf("JS", 6))
+// Tim index cua JS tai vi tri cuoi cung trong chuoi
+console.log(myString.lastIndexOf("JS"))
