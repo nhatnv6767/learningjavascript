f5e50819c66a27316329e392c9a28b56fb16b07b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-17 02:15:07 +0900

    Object - thêm key bằng 1 biến bên ngoài

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 // Object
 
+var emailKey = 'email';
 
 var myInfo = {
     name: 'Bi None',
     age: 18,
-    address: 'Ha Noi, Viet Nam'
+    address: 'Ha Noi, Viet Nam',
+    [emailKey]: 'abc@gmail.com'
 };
 
-var myKey = 'address'
 
-console.log(myInfo[myKey]);
+console.log(myInfo);
