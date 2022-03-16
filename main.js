3c80fae961747d6ea5e4a02c74986dbcada7dd7b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-17 00:45:15 +0900

    Array - Slice - Cat phan tu va chon het nhung phan tu con lai

--- main.js
+++ main.js
@@ -9,4 +9,4 @@ var languages = [
     'PHP'
 ]
 
-console.log(languages.slice(1, 2)) // chọn vị trí bắt đầu và cắt đến vị trí thứ 2 thì ngừng lại
+console.log(languages.slice(1)) // chọn vị trí bắt đầu và cắt đến vị trí thứ 2 thì ngừng lại
