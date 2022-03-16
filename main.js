a9b2ddd0bc2cbacba3e5cde1b1268f69b8c38ed0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-17 00:37:09 +0900

    Array - Slice - Chức năng chèn

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ var languages = [
     'PHP'
 ]
 
-languages.splice(1, 1)// tham số thứ nhất là vị trí con trỏ
+languages.splice(1, 0, 'Chen vao')// tham số thứ nhất là vị trí con trỏ
 // tham số thứ 2 là từ vị trí con trỏ xoá đi mấy phần tử
+// trường hợp xoá 0 phần tử nghĩa là chèn, thì có thêm 1 đối số mới 
+// là giá trị muốn chèn vào
 
 console.log(languages)
