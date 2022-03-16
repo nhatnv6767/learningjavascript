210cf3736809b537bc9d4aed2c496c010ae7f57f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-17 00:46:31 +0900

    Array - Slice - cắt toàn bộ mảng, giống với copy mảng

--- main.js
+++ main.js
@@ -9,4 +9,7 @@ var languages = [
     'PHP'
 ]
 
-console.log(languages.slice(1)) // chọn vị trí bắt đầu và cắt đến vị trí thứ 2 thì ngừng lại
+console.log(languages.slice(0)) // chọn vị trí bắt đầu và cắt đến vị trí thứ 2 thì ngừng lại 
+
+// slice(1) - cat tu vi tri 1 den cuoi mang
+// slice(0) - dung nhu chuc nang copy mang
