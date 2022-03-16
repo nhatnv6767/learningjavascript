7d378a1523a13eb5b4936f33e7885e992497fa99
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-17 00:04:28 +0900

    Array - Pop

--- main.js
+++ main.js
@@ -10,5 +10,6 @@ var languages = [
 ]
 
 console.log(languages.pop()) // xoá element cuối mảng và trả về phần tử đã xoá
+// khi mảng rỗng mà pop 1 lần nữa thì trả về undefined
 
 console.log(languages)
