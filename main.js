968c700e5e9570972749b8f4f2b76cca23934ebc
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-17 00:49:24 +0900

    Array - Slice - truyền số âm vào để lấy giá trị cuối

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ var languages = [
     'PHP'
 ]
 
-console.log(languages.slice(0)) // chọn vị trí bắt đầu và cắt đến vị trí thứ 2 thì ngừng lại 
+console.log(languages.slice(-2, -1)) // chọn vị trí bắt đầu và cắt đến vị trí thứ 2 thì ngừng lại 
 
 // slice(1) - cat tu vi tri 1 den cuoi mang
 // slice(0) - dung nhu chuc nang copy mang
+// truyền số âm vào để lấy phần tử cuối mảng
