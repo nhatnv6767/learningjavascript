dbdebc7f1d02dbb09b7f64686ec52bf3a7c58f3a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-17 01:16:22 +0900

    function - For of

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // Function
 
 function writeLog() {
-    console.log(arguments)
+    for (var param of arguments) {
+        console.log(param)
+    }
 }
 
-writeLog('Log 1', 'Log 2')
+writeLog('Log 1', 'Log 2', 'Log 3')
