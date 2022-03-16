37d080d6319801541037bc8938052314c6c9b3ab
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-17 00:39:30 +0900

    Array - Slice - Xoá đi và chèn vào nhiều phần tử

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var languages = [
     'PHP'
 ]
 
-languages.splice(1, 1, 'Xoa di va chen vao')// tham số thứ nhất là vị trí con trỏ
+languages.splice(1, 1, 'Xoa di va chen vao', 'Chen pt thu 2')// tham số thứ nhất là vị trí con trỏ
 // tham số thứ 2 là từ vị trí con trỏ xoá đi mấy phần tử
 // trường hợp xoá 0 phần tử nghĩa là chèn, thì có thêm 1 đối số mới 
 // là giá trị muốn chèn vào
