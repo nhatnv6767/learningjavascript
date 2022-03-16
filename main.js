a3a883e597c5c76126049033c8a8328825416d1b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-17 00:56:51 +0900

    function

--- main.js
+++ main.js
@@ -1,16 +1,7 @@
-// Array
+// Function
 
+function showDialog() {
+    alert('Hi xin chao cac ban');
+}
 
-var languages = [
-    'Not',
-    'To',
-    'Be',
-    'Javascript',
-    'PHP'
-]
-
-console.log(languages.slice(-2, -1)) // chọn vị trí bắt đầu và cắt đến vị trí thứ 2 thì ngừng lại 
-
-// slice(1) - cat tu vi tri 1 den cuoi mang
-// slice(0) - dung nhu chuc nang copy mang
-// truyền số âm vào để lấy phần tử cuối mảng
+showDialog()
