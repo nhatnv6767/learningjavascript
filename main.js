de0ee3f4df2475a63f27e30aed0c86d489c70a51
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-17 22:42:16 +0900

    Object constructor - call Object constructor inside Object

--- main.js
+++ main.js
@@ -11,5 +11,5 @@ var author = new User('None', 'Bi', 'Avatar');
 var user = new User('David', 'Kally', 'Avatar');
 // => author va user moi la đối tượng
 
-console.log(author);
+console.log(author.constructor);// gọi lại chính bản thiết kế
 console.log(user);
