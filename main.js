4e2cb11ffa074a1c4c46adfadbd401ca610af7cf
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-17 23:11:17 +0900

    Object prototype

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
-// Object constructor : Xay dung doi tuong
+// Object prototype: nguyên mẫu để tạo lên đối tượng
 
-// -> Object constructor
+
+// -> Object constructor: Bản thiết kế ngôi nhà
+// => Object prototype: nguyên liệu để tạo nên ngôi nhà
+
 function User(firstName, lastName, avatar) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.avatar = avatar; // => this ở đây để mô tả thuộc tính và phương thức sẽ có 
-    // cho đối tượng khi khởi tạo từ Object Constructor (User)
+    this.avatar = avatar; 
 
     this.getName = function() {
         return `${this.firstName} ${this.lastName}`// => this ở đây là this của getName
     }
 }
-// String, Number, Array, Date, ... chính là những Object constructor
-var author = new User('None', 'Bi', 'Avatar');
-var user = new User('David', 'Kally', 'Avatar');
-// => author va user moi la đối tượng
-
-author.title = 'Chia sẻ dạo tại FF'
-user.comment = 'Cho vay 5 chiệu'
-
-console.log(author.getName()); // this lúc này của author
-console.log(user.getName()); // this lúc này sẽ là của user
