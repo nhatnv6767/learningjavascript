ac88f86214bdd3706ccfbbac8a8bebcdd55840a0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-19 00:09:17 +0900

    Date

--- main.js
+++ main.js
@@ -1,26 +1,6 @@
-// Object prototype: nguyên mẫu để tạo lên đối tượng
+// Date
 
 
-// -> Object constructor: Bản thiết kế ngôi nhà
-// => Object prototype: nguyên liệu để tạo nên ngôi nhà
+var date = new Date();
 
-function User(firstName, lastName, avatar) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.avatar = avatar; 
-
-    this.getName = function() {
-        return `${this.firstName} ${this.lastName}`// => this ở đây là this của getName
-    }
-}
-
-User.prototype.className = 'FF'
-User.prototype.getClassName = function() {
-    return this.className;
-}
-
-var user = new User('Bi', 'None', 'Avatar 1')
-var user2 = new User('Nguyen', 'Vu', 'Avatar 2')
-
-console.log(user.className) // => undefined
-console.log(user2.getClassName())
+console.log(typeof date) // object
