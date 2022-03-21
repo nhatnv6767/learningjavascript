f41ed83a5d741c4b51b2d34b789aab51d5839afa
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-22 00:07:14 +0900

    Array - map() - tham số trong function of map

--- main.js
+++ main.js
@@ -43,13 +43,14 @@ var courses = [
 // map() - sử dụng khi muốn thay đổi, chỉnh sửa element của array
 // tạo ra 1 mảng mới có số lượng phần tử bằng với số lượng phần tử mảng cũ
 
-function courseHandler(course, index) {
+function courseHandler(course, index, originArray) {
     return {
         id: course.id,
         name: `Khoa hoc: ${course.name}`,
         coin: course.coin,
         coinText: `Gia: ${course.coin}`,
-        index: index
+        index: index,
+        originArray: originArray
     }
 }
 
