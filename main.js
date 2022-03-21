52f67300323f3478c02379ed6ab9b010d28c4868
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-21 23:55:14 +0900

    Array - map() - function ngoai` - cach khac

--- main.js
+++ main.js
@@ -43,8 +43,8 @@ var courses = [
 // map() - sử dụng khi muốn thay đổi, chỉnh sửa element của array
 // tạo ra 1 mảng mới có số lượng phần tử bằng với số lượng phần tử mảng cũ
 
-function courseHandler() {
-
-};
+var courseHandler = function() {
+    
+}
 
 var newCourses = courses.map(courseHandler);
