16242de81272095cd98f54ed44d9f7f05f79d502
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-21 23:22:21 +0900

    Array - 2

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-// nested loop
+// Array
 
+/**
+ * Array methods
+ * forEach()
+ * every()
+ * some()
+ * find()
+ * filter()
+ * map()
+ * reduce()
+ * 
+ */
 
-var myArray = [
-    [1, 2],
-    [3, 4],
-    [5, 6],
+var courses = [
+    {
+        id: 1,
+        name: 'Javascript',
+        coin: 250
+    },
+    {
+        id: 2,
+        name: 'HTML, CSS',
+        coin: 0
+    },
+    {
+        id: 3,
+        name: 'Ruby',
+        coin: 0
+    },
+    {
+        id: 4,
+        name: 'ReactJS',
+        coin: 500
+    },
 ];
-
-for (var i = 0; i < myArray.length; i++) {
-    for (var j = 0; j < myArray[i].length; j++) {
-        console.log(myArray[i][j])
-    }
-}
