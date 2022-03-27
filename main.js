b6ad6f5375eae72a9a90f9d08ee742397565d660
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-28 00:04:33 +0900

    sort()

--- main.js
+++ main.js
@@ -1,36 +1,3 @@
-// Callback 
-
-// Là hàm (function) được truyền qua đối số 
-// khi gọi hàm khác
-
-// 1. Là hàm
-// 2. Được truyền qua đối số
-// 3. Được gọi lại (trong hàm nhận đối số)
-
-Array.prototype.map2 = function (callback) {
-    var output = [], arrayLength = this.length
-    for (var i = 0; i < arrayLength; i++) {
-        var result = callback(this[i], i)
-        output.push(result)
-    }
-    return output
-}
-
-var courses = [
-    'Javascript',
-    'PHP',
-    'Ruby'
-];
-
-var htmls = courses.map2(function (course, index) {
-    return `<h2>${course}</h2>`;
-});
-
-console.log(htmls);
-
-
-// var htmls = courses.map(function (course) {
-//     return `<h2>${course}</h2>`
-// })
-
-// console.log(htmls.join(''))
+let numbers = [4, 2, 5, 1, 3];
+numbers.sort((a, b) => a - b);
+console.log(numbers);
