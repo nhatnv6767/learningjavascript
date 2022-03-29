3067ef6bed150b77c2a2eebcde5b531e8f8f5b38
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-29 23:09:46 +0900

    Promise

--- main.js
+++ main.js
@@ -1,29 +1,9 @@
-const callback = (error, data) => {
-    if (error) {
-        console.log('>>> calling callback with error: ', error)
-    }
-    if (data) {
-        console.log('>>> calling callback with data: ', data)
-    }
+const promiseExp = () => {
+    return new Promise((resolve, reject) => {
+        resolve('get some data')
+    })
 }
 
-function getTodos(id, callback) {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if (this.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText);
-            const dataString = JSON.stringify(data);
-            callback(undefined, dataString);
-        }
-
-        if (this.readyState === 4 && request.status !== 200) {
-            callback('Something wrongs', undefined);
-        }
-    }
-
-    request.open('GET',`https://jsonplaceholder.typicode.com/todos/${id}`, true);
-    request.send();
-}
-
-getTodos(1, callback)
-getTodos(3, callback)
+promiseExp().then(data => {
+    console.log(data)
+})
