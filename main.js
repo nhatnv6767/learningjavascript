05c683f63848ec2f2a293ed17004f5080fb274ec
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-30 02:22:30 +0900

    Destructuring assignment - Gian luoc code

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 /// ... Destructuring assignment ...
 let arr1 = [1, 2, 3, 4, 5]
+let state = {
+    name: 'None',
+    address: 'Nhat Ban',
+    channel: 'JapanJAV'
+}
 
 console.log('>>> Check data arr1: ',arr1)
 
-let arr2 = [0, ...arr1, 6]
+let name = state.name;
+let address = state.address;
+let channel = state.channel;
 
-console.log('>>> Check data arr2: ',arr2)
+console.log('>>> Check key: ',name, address, channel)
