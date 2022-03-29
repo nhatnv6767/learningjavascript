600191c25831d274090e2b87d6b95e34b2738f1f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-29 22:22:11 +0900

    JS Advanced

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
-let numbers = [4, 2, 5, 1, 3];
-numbers.sort((a, b) => a - b);
-console.log(numbers);
+const callback = (error, data) => {
+    if (error) {
+        console.log('>>> calling callback with error: ', error)
+    }
+    if (data) {
+        console.log('>>> calling callback with data: ', data)
+    }
+}
+
+function getTodos(callback) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState === 4 && request.status === 200) {
+            const data = JSON.parse(request.responseText);
+            const dataString = JSON.stringify(data);
+            callback(undefined, dataString);
+        }
+
+        if (this.readyState === 4 && request.status !== 200) {
+            callback('Something wrongs', undefined);
+        }
+    }
+
+    request.open('GET','https://jsonplaceholder.typicode.com/todos', true);
+    request.send();
+}
+
+getTodos(callback)
