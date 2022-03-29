418e59a455df432494104c2f9bdf0a8751127240
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-30 01:56:05 +0900

    Throwing Errors 2

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-const getNewTodo = async(id) => {
+const getNewTodo = async (id) => {
     let response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
     if (response && response.status !== 200) {
-        return new Error('Something wrongs with status code: ' + response.status);
+        throw new Error('Something wrongs with status code: ' + response.status);
     }
     let data = await response.json();
     return data;
 }
 
-getNewTodo(4555).then(data => {
-    console.log('>>>> check get data: ', data);
-})
+getNewTodo(4555)
+    .then(data => {
+        console.log('>>>> check get data: ', data);
+    })
+    .catch(error => {
+        console.log('>>>> error: ', error);
+    })
