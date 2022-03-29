6bfbf7e80de14b20691d00f8b4e93f3b84650034
nhatnv6767 <nhatnvse90183@gmail.com>
2022-03-30 01:53:05 +0900

    Throwing Erros

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const getNewTodo = async(id) => {
     let response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    if (response && response.status !== 200) {
+        return new Error('Something wrongs with status code: ' + response.status);
+    }
     let data = await response.json();
     return data;
 }
 
-getNewTodo(4).then(data => {
+getNewTodo(4555).then(data => {
     console.log('>>>> check get data: ', data);
 })
